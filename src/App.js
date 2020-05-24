3d426d868f39b4af4835fe9a26898e6245c8c641
Kaj Białas <kaj.bialas@dev-industry.com>
2020-05-24 16:22:00 +0200

    Issue 7

--- src/App.js
+++ src/App.js
@@ -7,23 +7,31 @@ import Users from './Users';
 import './App.css';
 
 export const UserContext = React.createContext('');
+export const CounterContext = React.createContext(0);
 
 function App() {
   const [userEmail, setUserEmail] = useState('');
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <UserContext.Provider value={{
         userEmail: userEmail,
         login: () => setUserEmail('kaj.bialas@gmail.com')
       }} >
-      <div>
-        {userEmail ? userEmail : <button onClick={() => setUserEmail('kaj.bialas@gmail.com')}>ZALOGUJ SIE</button>}
-      </div>
-      <Header />
-      <Counter />
-      <CounterF/>
-      <WindowResize />
-      <Users />
+        <CounterContext.Provider value={{
+          counterValue: counter,
+          incrementCounter: () => setCounter(counter+1)
+        }}>
+          <div>
+            {userEmail ? userEmail : <button onClick={() => setUserEmail('kaj.bialas@gmail.com')}>ZALOGUJ SIE</button>}
+          </div>
+          <Header />
+          <Counter />
+          <CounterF/>
+          <WindowResize />
+          <Users />
+        </CounterContext.Provider>
       </UserContext.Provider>
     </div>
   );
--- src/CounterF.js
+++ src/CounterF.js
@@ -1,28 +1,14 @@
-import React, { useReducer } from 'react';
-
-
-function counterReducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        value: state.value + 1};
-    case 'DECREMENT':
-      return {value: state.value - 1};
-    case 'RESET':
-      return {value: 0};
-  }
-};
+import React, { useReducer, useContext } from 'react';
+import { CounterContext } from './App';
 
 function CounterF() {
-  const [counterState, dispatch] = useReducer(counterReducer, {value: 0});
+  const {counterValue, incrementCounter} = useContext(CounterContext)
 
   return (
     <div>
       <h2>Counter:</h2>
-      <div>{counterState.value}</div>
-      <button onClick={() => dispatch({type: 'INCREMENT'})}>+</button>
-      <button onClick={() => dispatch({type: 'DECREMENT'})}>-</button>
-      <button onClick={() => dispatch({type: 'RESET'})}>RESET</button>
+      <div>{counterValue}</div>
+      <button onClick={incrementCounter}>+</button>
     </div>
   )
 }
