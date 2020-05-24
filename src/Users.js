23943f510c5780b927c3e12f3c5e2ff415b527c0
Kaj Białas <kaj.bialas@dev-industry.com>
2020-05-24 13:32:15 +0200

    issue 5 start

--- src/Users.js
+++ src/Users.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 function Users() {
-  const [users, setUsers] = useState([]);
+  const [usersData, setUsers] = useState([]);
+  const [usersLoading, setUsersLoading] = useState(false);
+  const [usersError, setUsersError] = useState(false);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users') 
+    setUsersLoading(true);
+    fetch('https://jsonplaceho') 
       .then(response => response.json()) 
-      .then(json => setUsers(json));
+      .then(json => setTimeout(() => {
+        setUsersLoading(false);
+        setUsers(json);
+      }
+      , 3000));
   }, []);
 
-  const renderUsers = () => users.map((user) => <div key={user.id}>{user.name}</div>);
+  const renderUsers = () => usersData.map((user) => <div key={user.id}>{user.name}</div>);
 
   return(
     <div>
       <h2>Użytkownicy</h2>
+      {usersLoading && <div>Ładowanie danych</div>}
       {renderUsers()}
     </div>
   )
